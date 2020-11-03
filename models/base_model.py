05756c9bd86d8080302869e934f905e08a5b6265
juanjts <1843@holbertonschool.com>
2020-11-03 15:54:22 +0000

    basemodel changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ BaseModel that defines all common attributes/methods for other classes """
-
-
 import uuid
 import datetime
-from models.__init__ import storage
+import models
 
 
 class BaseModel:
@@ -13,29 +11,44 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
+        """initialize method"""
+
         self.id = str(uuid.uuid4())
         if kwargs:
             for key, value in kwargs.items():
-                if key is  "__class__":
+                if key is "__class__":
                     continue
                 elif key in ("created_at", "updated_at"):
-                   exec("self." + key + "= datetime.datetime.strptime( \"" + value + "\", \"%Y-%m-%dT%H:%M:%S.%f\")")
+                    exec("self." + key + "= datetime.datetime.\
+                        strptime(\""+value+"\", \"%Y-%m-%dT%H:%M:%S.%f\")")
                 else:
                     self.key = value
         else:
-            self.created_at = datetime.datetime.today()
-            self.updated_at = datetime.datetime.today()
-            storage.new(self)
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
+        """print name of class, id instance and dict"""
+
         return "[" + str(self.__class__.__name__) + "]" + " (" + self.id + ")"\
-            + " " + str( self.__dict__)
+            + " " + str(self.__dict__)
 
     def save(self):
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
+        """
+
         self.updated_at = datetime.datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
+        """
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
+        """
+
         dictcpy = self.__dict__.copy()
         dictcpy["__class__"] = str(self.__class__.__name__)
         dictcpy["created_at"] = datetime.datetime.isoformat(self.created_at)
